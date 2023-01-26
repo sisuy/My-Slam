62d07e15f0fb99690b6de24bde734522af5fc18c
sisuy <1808028940@qq.com>
2023-01-26 16:31:58 +1300

    Improve: use ransac to improve the performance in camera pose

--- slam.py
+++ slam.py
@@ -124,18 +124,20 @@ class Slam:
         f1.match_points = [cv2.KeyPoint(x = i[0], y = i[1], size = None) for i in match_points1]
         f2.match_points = [cv2.KeyPoint(x = i[0], y = i[1], size = None) for i in match_points2]
         
-        print('-------------- Essential matrix -----------------------')
-        # Rebuild the essential matrix
-        U,S,VT = np.linalg.svd(model.params)
-        print(np.linalg.det(U))
-        rebuild_S = np.array([[S[0],0,0],
-                              [0,S[1],0],
-                              [0,  0 ,0]])
-        print('------------------------------------------------------')
 
         # recover pose
-        E = cv2.findEssentialMat(pts_ransac1,pts_ransac2,self.K,cv2.RANSAC)
-        _,R,T,mask_match = cv2.recoverPose(E[0], pts_ransac1, pts_ransac2,self.K)
+        E = cv2.findEssentialMat(match_points1,match_points2,self.K,cv2.RANSAC)[0]
+
+        # print('-------------- Essential matrix -----------------------')
+        # # Rebuild the essential matrix
+        # U,S,VT = np.linalg.svd(E)
+        # rebuild_S = np.array([[S[0],0,0],
+        #                       [0,S[1],0],
+        #                       [0,  0 ,0]])
+        # E = np.dot(np.dot(U,rebuild_S),VT)
+        # print('------------------------------------------------------')
+        
+        _,R,T,mask_match = cv2.recoverPose(E, match_points1, match_points2,self.K)
         RT = helper.poseRt(R,T.T)
 
         return [f2,RT]
